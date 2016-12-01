dcb0df9c6a71bee3355f2ee847228a922e1e98ae
HansiMou <mhsasd@126.com>
2016-12-01 14:26:15 -0500

    uglify

--- dist/service-worker.js
+++ dist/service-worker.js
@@ -8,7 +8,7 @@ var urlsToCache = [
   '//yoav-zibin.github.io/TicTacToe/dist/js/everything.min.js',
   '//yoav-zibin.github.io/TicTacToe/dist/css/everything.min.css',
 ];
-var CACHE_NAME = 'cache-v2016-12-01T16:53:57.050Z';
+var CACHE_NAME = 'cache-v2016-12-01T19:24:47.921Z';
 
 self.addEventListener('activate', function(event) {
   event.waitUntil(
